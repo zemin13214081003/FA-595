b77f631f2c87a2d82fd1fc63e5e4a5c9dcb33812
zemin13214081003 <90585667+zemin13214081003@users.noreply.github.com>
2021-11-08 00:14:29 -0500

    Add files via upload

---
+++ introduce to Git.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#set the period of 2^2
+x=np.arrange(0,2^2.np,0.01)
+y.sin=np.sin(x)
+y.cos=np.cos(x)
+#plot them
+plt.plot(x,y.sin)
+plt.plot(x,y.cos)
+plt.show()
